44ba8cecb8e61bac7e4fe9100072a7b17aca7a79
OZimmermann <82403411+OZimmermann@users.noreply.github.com>
2021-05-01 11:45:00 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,19 @@
+while (true) {
+    if (input.buttonIsPressed(Button.A)) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            # . . . #
+            . . . . .
+            # . . . #
+            `)
+        basic.pause(1000)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+}
